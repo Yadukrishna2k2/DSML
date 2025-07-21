9d0e8d4b67c6a5df840621de644c097400cfd9c4
Yadukrishna2k2 <yadukrishna2k2@gmail.com>
2025-07-21 15:24:29 +0530

    Update barchart.py

--- barchart.py
+++ barchart.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 languages = ['Java','Python','PHP','JavaScript','C#','C++']
 popularity = [22.2, 17.6, 8.8, 8.0, 7.7, 6.7]
-
-plt.bar(languages, popularity, color='skyblue')
+x=np.array(languages)
+y=np.array(popularity)
+plt.bar(x ,y, color='skyblue')
 
 plt.xlabel("Programming Languages")
 plt.ylabel("Popularity (%)")
